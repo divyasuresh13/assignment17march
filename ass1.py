a929070266f603245f6bddf5e531c21392992706
Divya Suresh <79002465+divyasuresh13@users.noreply.github.com>
2021-03-17 12:59:43 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,15 @@
+class Vehicle: 
+    def __init__(self): 
+        print("Hello!!! This is Honda CBR 1000RR") 
+  
+    def  max_speed(self): 
+        Speed=299 
+        print("\n Max_speed:",Speed) 
+  
+    def mileage(self): 
+        mil = 18
+        print("\n Milage:", mil)
+  
+s = Vehicle() 
+s.max_speed() 
+s.mileage() 
